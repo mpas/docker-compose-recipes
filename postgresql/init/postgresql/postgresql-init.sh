5b6ba7ce0fed6d515f0f30f12929134f80adad47
Marco Pas <139094+mpas@users.noreply.github.com>
2022-02-25 13:53:23 +0100

    Update postgres recipe

--- postgresql/init/postgresql/postgresql-init.sh
+++ postgresql/init/postgresql/postgresql-init.sh
@@ -6,14 +6,10 @@ echo "Running PostgreSQL initialization script"
 echo "--------------------------------------------------------------------------------"
 
 # execute some commands on postgresql initialization
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 <<-EOSQL
 
   -- CREATE USER $EXAMPLE_USER WITH PASSWORD '$EXAMPLE_USER_PASSWORD';
   -- CREATE DATABASE $EXAMPLE_DATABASE;
   -- GRANT ALL PRIVILEGES ON DATABASE $EXAMPLE_DATABASE TO $EXAMPLE_USER;
-
-  -- Cleanup!
-  -- We do not need the default postgres db, so we drop it
-  DROP DATABASE postgres;
 
 EOSQL
